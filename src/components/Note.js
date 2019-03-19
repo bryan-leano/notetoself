722f90c2c97b24768fad37caf5a66fc09e02a123
Bryan Leanoe <bryan.leano@gmail.com>
2019-03-18 21:49:52 -0600

    recommit

---
+++ src/components/Note.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Note extends React.Component {
+  render() {
+    return (
+      <div className='note'>
+        <p>{this.props.note.text}</p>
+      </div>
+    )
+  }
+}
+
+export default Note;
